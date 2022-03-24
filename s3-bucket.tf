51b2fcdffcf779be69818f464b640a4b6b14da1a
Madhu <madhuperam29@gmail.com>
2022-03-24 19:50:33 +0000

    Added s3 bucket module wih prefix correction

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -2,5 +2,5 @@ module "s3-bucket" {
   source  = "app.terraform.io/TCS-Madhu/s3-bucket/aws"
   version = "2.8.0"
   # insert required variables here
-  bucket_prefix = "Madhu"
+ bucket_prefix = "madhu"
 }
